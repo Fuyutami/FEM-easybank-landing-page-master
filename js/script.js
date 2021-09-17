24870931ac042f78a40ee8e94bfb09c7cfb0d5ca
[Fuyutami] <fuyutami.art@gmail.com>
2021-09-17 15:21:49 +0300

    implement mobile hamburger menu, finish the project

---
+++ js/script.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const mobileMenu = document.querySelector('.mobile-navigation')
+const btnMobileMenu = document.querySelector('.header__mobile-menu')
+const iconMobileMenu = document.querySelector('.header__mobile-menu-icon')
+
+
+
+
+btnMobileMenu.addEventListener('click', function () {
+
+    if(mobileMenu.classList.contains('hidden')) {
+        mobileMenu.classList.remove('hidden')
+        iconMobileMenu.src =  'images/icon-close.svg'
+    }
+    else {
+        mobileMenu.classList.add('hidden')
+        iconMobileMenu.src =  'images/icon-hamburger.svg'
+    }
+})
